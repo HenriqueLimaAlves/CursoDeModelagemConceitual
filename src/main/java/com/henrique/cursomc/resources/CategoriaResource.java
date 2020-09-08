f6e5c0ab1fec2f670735ed1cc8560581ed2d1f77
Hnerique <henri633@gmail.com>
2020-09-07 22:57:25 -0300

    Testando o dominio - Categoria

--- src/main/java/com/henrique/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/henrique/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.henrique.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.henrique.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,7 +15,15 @@ public class CategoriaResource {
 	
 	//@RequestMapping(method=RequestMethod.GET)
 	@GetMapping
-	public String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
