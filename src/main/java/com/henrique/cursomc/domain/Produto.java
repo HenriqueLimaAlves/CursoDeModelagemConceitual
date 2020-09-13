b64d1fed2413f63bf1cd03a662b5e5cc8e1cc0e6
Hnerique <henri633@gmail.com>
2020-09-13 16:14:27 -0300

    Associacao ManyToMany

--- src/main/java/com/henrique/cursomc/domain/Produto.java
+++ src/main/java/com/henrique/cursomc/domain/Produto.java
@@ -1,11 +1,16 @@
 package com.henrique.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -18,6 +23,25 @@ public class Produto implements Serializable{
 	private Integer id;
 	private String nome;
 	private Double preco;
+	
+	@ManyToMany
+	@JoinTable(name="Produto_Categoria", 
+	joinColumns = @JoinColumn(name="produto_id"), 
+	inverseJoinColumns = @JoinColumn(name = "categoria_id" ))
+	
+	private List<Categoria> categorias = new ArrayList<>();
+	
+	public Produto() {
+		
+	}
+	
+	public Produto(Integer id, String nome, Double preco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+	}
+
 	public Integer getId() {
 		return id;
 	}
@@ -37,13 +61,16 @@ public class Produto implements Serializable{
 		this.preco = preco;
 	}
 
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
+	public List<Categoria> getCategorias() {
+		return categorias;
 	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+
 	
+	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
